10240043ad080224a427d7a237e4d0f640dbccb0
George Barta <george.barta@boeing.com>
2017-03-10 02:55:25 -0800

    Added authentication

--- sample_run.sh
+++ sample_run.sh
@@ -4,4 +4,7 @@ export TWILIO_ACCOUNT_SID=<<ID>>
 export TWILIO_ACCOUNT_AUTH=<<AUTH>>
 export ADMIN_NUMBER=<<TEL_NUM>>
 
+export SECURE_USER=<<user>>
+export SECURE_PASSWORD=<<password>>
+
 node server.js
