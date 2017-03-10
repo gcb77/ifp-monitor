20a06649994e4ffa959f2d75c2b6d5d0d5c85591
George Barta <george.barta@boeing.com>
2017-03-10 04:11:35 -0800

    Better navigation, form for adding users

--- server.js
+++ server.js
@@ -35,27 +35,66 @@ function getUnauthorizedResponse(req) {
 }
 
 app.get('/', function(req, res) {
-  res.send("<a href='/monitor'>Monitor</a><br>" +
-  "<a href='/start'>Start</a><br>" +
-  "<a href='/stop'>Stop</a><br>" +
-  "<hr>Monitoring: " +
-
-  JSON.stringify(monitor.getStats().monitoredPlayers)
-  )
+  var send = []
+  send.push("<a href='/monitor'>Server Status</a><br>")
+  send.push("<a href='/players'>Players Monitored</a><br>")
+  if(monitor.getStats().started) {
+    send.push("<a href='/stop'>Stop</a><br>")
+  } else {
+    send.push("<a href='/start'>Start</a><br>")
+  }
+  send.push("<hr>Monitoring: ")
+  send.push(JSON.stringify(monitor.getStats().monitoredPlayers))
+  res.send(send.join(''))
 })
 
 app.get('/monitor', function(req,res) {
   res.send(monitor.getStats())
 })
 
-app.get('/addPlayer/:name/:number', function(req,res) {
-  monitor.addMonitoredPlayer(req.params.name, req.params.number)
-  res.send(monitor.getStats().monitoredPlayers)
+app.get('/setMonitorStatus/:name/:status', function(req, res) {
+  monitor.setMonitorStatusForPlayer(req.params.name, JSON.parse(req.params.status))
+  res.redirect('/players')
+})
+
+app.get('/players', function(req, res) {
+  var retBuf = []
+
+  retBuf.push('<a href="/">Home</a>')
+
+  retBuf.push('<form method="GET" action="addPlayer">')
+  retBuf.push('NAME: <input type=text name="name">')
+  retBuf.push('NUMBER: <input type=text name="number">')
+  retBuf.push('<input type="submit">')
+  retBuf.push('</form>')
+
+  var players = monitor.getMonitoredPlayers()
+  Object.keys(players).forEach(function(player) {
+    retBuf.push(player)
+    if(players[player].enabled) {
+      retBuf.push("<a href='/setMonitorStatus/" + encodeURIComponent(player) + "/false'>Disable</a>")
+    } else {
+      retBuf.push("<a href='/setMonitorStatus/" + encodeURIComponent(player) + "/true'>Enable</a>")
+    }
+    retBuf.push('<br>')
+  })
+
+  res.send(retBuf.join('\n'))
+})
+
+app.get('/addPlayer', function(req,res) {
+  try {
+    monitor.addMonitoredPlayer(req.query.name, req.query.number)
+  } catch (err) {
+    res.send(err.message)
+    return
+  }
+  res.redirect('/players')
 })
 
 app.get('/start', function(req,res) {
   monitor.monitorStart()
-  res.send("Started")
+  res.send("Started<br><a href='/'>Home</a>")
 })
 
 app.get('/stop', function(req,res) {
