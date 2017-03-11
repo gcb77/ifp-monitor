a1198273e6797f81659a429fea01024056db80e4
George Barta <george.barta@boeing.com>
2017-03-11 11:07:59 -0800

    Added style

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ function getUnauthorizedResponse(req) {
 
 app.get('/', function(req, res) {
   var send = []
-  send.push("<a href='/monitor'>Server Status</a><br>")
-  send.push("<a href='/players'>Players Monitored</a><br>")
+  send.push(fs.readFileSync('head.html'))
+  send.push(fs.readFileSync('nav.html'))
   if(monitor.getStats().started) {
-    send.push("<a href='/stop'>Stop</a><br>")
+    send.push("<a href='/stop' class='btn'>Stop</a><br>")
   } else {
-    send.push("<a href='/start'>Start</a><br>")
+    send.push("<a href='/start' class='btn'>Start</a><br>")
   }
   send.push("<hr>Monitoring: ")
   send.push(JSON.stringify(monitor.getStats().monitoredPlayers))
@@ -49,7 +49,20 @@ app.get('/', function(req, res) {
 })
 
 app.get('/monitor', function(req,res) {
-  res.send(monitor.getStats())
+  var send = []
+  send.push(fs.readFileSync('head.html'))
+  send.push(fs.readFileSync('nav.html'))
+  var stats = monitor.getStats()
+  send.push("<ul>")
+  send.push("<li><b>Monitoring "+stats.monitoredPlayers.length + " players </b>")
+  send.push("<li><b>Notifications Sent: </b>"+stats.notificationsSent)
+  if(stats.started) {
+    send.push("<li><b>Running for: </b>"+Math.round((Date.now() - stats.started)/1000/60) + " minutes")
+  }
+  send.push("</ul>")
+  send.push("</hr>")
+  send.push(JSON.stringify(stats))
+  res.send(send.join('\n'))
 })
 
 app.get('/setMonitorStatus/:name/:status', function(req, res) {
@@ -60,7 +73,8 @@ app.get('/setMonitorStatus/:name/:status', function(req, res) {
 app.get('/players', function(req, res) {
   var retBuf = []
 
-  retBuf.push('<a href="/">Home</a>')
+  retBuf.push(fs.readFileSync('head.html'))
+  retBuf.push(fs.readFileSync('nav.html'))
 
   retBuf.push('<form method="GET" action="addPlayer">')
   retBuf.push('NAME: <input type=text name="name">')
@@ -68,17 +82,24 @@ app.get('/players', function(req, res) {
   retBuf.push('<input type="submit">')
   retBuf.push('</form>')
 
+  retBuf.push('<div class="row">')
   var players = monitor.getMonitoredPlayers()
   Object.keys(players).forEach(function(player) {
+    retBuf.push('<div class="col-md-2">')
     retBuf.push(player)
+    retBuf.push('</div>')
+    retBuf.push('<div class="col-md-2">')
     if(players[player].enabled) {
       retBuf.push("<a href='/setMonitorStatus/" + encodeURIComponent(player) + "/false'>Disable</a>")
     } else {
       retBuf.push("<a href='/setMonitorStatus/" + encodeURIComponent(player) + "/true'>Enable</a>")
     }
+    retBuf.push('</div>')
     retBuf.push('<br>')
   })
+  retBuf.push('</div>')
 
+  retBuf.push('</body>')
   res.send(retBuf.join('\n'))
 })
 
@@ -94,12 +115,14 @@ app.get('/addPlayer', function(req,res) {
 
 app.get('/start', function(req,res) {
   monitor.monitorStart()
-  res.send("Started<br><a href='/'>Home</a>")
+  res.redirect('/')
+  // res.send("Started<br><a href='/'>Home</a>")
 })
 
 app.get('/stop', function(req,res) {
   monitor.monitorStop()
-  res.send("Stopped")
+  res.redirect('/')
+  // res.send("Stopped")
 })
 
 app.listen(port)
